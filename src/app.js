61399e32e50c18fc5ea1483041dd395598c8e158
evfsmart <evfsmart@gmail.com>
2021-05-18 19:15:14 +0100

    Displayed six-day forecast function in JS using the API

--- src/app.js
+++ src/app.js
@@ -14,24 +14,32 @@ function formatDate(timestamp) {
 }
 // timestamp = miliseconds since Monday January 19th 1970//
 
+function formatDay (timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+    return days[day];
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
-let forecastElement = document.querySelector("#forecast");
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+    let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
 
-let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-
-let forecastHTML = `<div class="row">`;
-days.forEach(function(day) {
-forecastHTML = forecastHTML + `
-        <div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
-            <img src="https://openweathermap.org/img/wn/50d@2x.png" alt="" width="42"/>
-            <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-temperature-max">18º</span>
-                <span class="weather-forecast-temperature-min">12º</span>
-            </div>
-            </div>
-        `;
+    let forecastHTML = `<div class="row">`;
+    forecast.forEach(function(forecastDay, index) {
+        if (index < 6) {
+        forecastHTML = forecastHTML + `
+            <div class="col-2">
+                <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+                <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"/>
+                <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}º</span>
+                    <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}º</span>
+                </div>
+                </div>
+            `;
+          }
         });
         forecastHTML = forecastHTML + `</div>`;
         forecastElement.innerHTML = forecastHTML;
@@ -40,7 +48,6 @@ forecastHTML = forecastHTML + `
 function getForecast(coordinates) {
     let apiKey = "17a639a638a4bfe25417abb69ec4868d";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-    console.log(apiUrl);
     axios.get(apiUrl).then(displayForecast);
 }
 
