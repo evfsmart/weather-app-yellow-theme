c19534e7cb39476c8e468a1b8c4719c73accf750
evfsmart <evfsmart@gmail.com>
2021-05-18 20:40:17 +0100

    Added DinosoftLabs weather icons

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ function displayForecast(response) {
         forecastHTML = forecastHTML + `
             <div class="col-2">
                 <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-                <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"/>
+                <img src="img/${forecastDay.weather[0].icon}.png" alt="" width="42"/>
                 <div class="weather-forecast-temperatures">
                     <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}º</span>
                     <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}º</span>
@@ -65,7 +65,7 @@ celsiusTemperature = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
 dateAndUpdatedElement.innerHTML = formatDate(response.data.dt * 1000);
 descriptionElement.innerHTML = response.data.weather[0].description;
-iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("src", `img/${response.data.weather[0].icon}.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
 humidityElement.innerHTML = response.data.main.humidity;
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
